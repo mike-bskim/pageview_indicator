5fa4cd46109f07b20a280be2de53679e433d3a61
mike-bskim <mike-bskim@gmail.com>
2021-05-11 22:37:38 +0200

    indicator position

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-
         primarySwatch: Colors.blue,
       ),
       home: TopBannerPage(),
@@ -24,22 +23,99 @@ class TopBannerPage extends StatefulWidget {
   _TopBannerPageState createState() => _TopBannerPageState();
 }
 
+final _itemCount = 7;
+Size size;
+
 class _TopBannerPageState extends State<TopBannerPage> {
-  final _itemCount = 7;
+  PageController _pageController = PageController();
+  LittleDotsPainter _littleDotsPainter = LittleDotsPainter(_itemCount, 0);
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _pageController.addListener(onPageChanged);
+  }
+
+  void onPageChanged() {
+    setState(() {
+      _littleDotsPainter = LittleDotsPainter(_itemCount, _pageController.page);
+    });
+//    print(_pageController.page);
+  }
+
   @override
   Widget build(BuildContext context) {
+    if(size == null) {
+      size = MediaQuery.of(context).size;
+    }
+
+//    print(size.width.toString());
     return Scaffold(
       body: Stack(
         children: <Widget>[
-          PageView.builder(
-            itemBuilder: (BuildContext context, int index){
-              return Image.network('https://picsum.photos/200/300', fit: BoxFit.cover,);
-            },
-            itemCount: _itemCount,
-          ),
+          _images(),
+          Positioned(
+            left: size.width / 2 - _itemCount * 5,
+//            left: 100,
+            right: size.width / 2 - _itemCount * 5,
+            bottom: 90,
+            height: size.width / _itemCount / 2,
+            child: CustomPaint(
+              painter: _littleDotsPainter,
+            ),
+          )
         ],
       ),
-
     );
+  }
+
+  PageView _images() {
+    return PageView.builder(
+      controller: _pageController,
+      itemBuilder: (BuildContext context, int index) {
+        return Image.network(
+          'https://picsum.photos/200/300',
+          fit: BoxFit.cover,
+        );
+      },
+      itemCount: _itemCount,
+    );
+  }
+}
+
+class LittleDotsPainter extends CustomPainter {
+  final int numOfDots;
+  final double page;
+
+  LittleDotsPainter(this.numOfDots, this.page);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    // TODO: implement paint
+    for (int i = 0; i < numOfDots; i++) {
+      canvas.drawCircle(
+          Offset(size.width / numOfDots * i*1.5 + (size.width / numOfDots / 2), 0),
+//          Offset((i*15*3).toDouble(), 0),
+//          10,
+          size.width / _itemCount / 3, //반지름
+          Paint()
+            ..color = Colors.red
+            ..strokeWidth = 2
+            ..style = PaintingStyle.stroke);
+    }
+    print('width: ${size.width}, numOfDots: $numOfDots');
+    canvas.drawCircle(
+        Offset(size.width / numOfDots * page*1.5 + (size.width / numOfDots / 2), 0),
+        size.width / _itemCount / 3,
+        Paint()
+          ..color = Colors.greenAccent);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    // TODO: implement shouldRepaint
+//    throw UnimplementedError();
+    return (oldDelegate as LittleDotsPainter).page != page;
   }
 }
